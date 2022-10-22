398dc17db0095e03466b70f8f8f605ad2973376b
Saksham Attri <sakshamattri3@gmail.com>
2022-10-23 02:38:42 +0530

    Edit infixToPrefix.h

--- Task1/infixToPrefix.h
+++ Task1/infixToPrefix.h
@@ -1,1 +1,11 @@
+/*! \file infixToPrefix.h
+    \brief A documented header file
+    
+    Contains declaration for infixToPrefix()
+*/
+/*! \fn  int infixToPrefix(char * infix, char * prefix)
+    \brief Reads \a infix and generates it's prefix which is written to \a prefix . Returns length of \a prefix .
+    \param infix The input string of infix expression
+    \param prefix The output string
+*/
 int infixToPrefix(char * infix, char * prefix);
