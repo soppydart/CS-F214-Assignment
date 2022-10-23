beeb067aae21ed0dfef89254932e54ecf07f5fe9
Saksham Attri <sakshamattri3@gmail.com>
2022-10-23 16:51:23 +0530

    Add Task1 code

--- Task1/infixToPrefix.h
+++ Task1/infixToPrefix.h
@@ -7,6 +7,8 @@
     \brief Reads \a infix and generates it's prefix which is written to \a prefix . 
     \param infix The input string of infix expression
     \param prefix The output string
+    \param stack The stack for in-function operations
+    \param lenInfix Length of the infix expression
     \return Returns length of \a prefix.
 */
-int infixToPrefix(char * infix, char * prefix);
+int infixToPrefix(char * infix, char * stack, char * prefix, int lenInfix);
