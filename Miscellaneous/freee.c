a6077403328ccbdc60515025af998a4e1e8c3fd4
333pranav <pranav03.sharma@gmail.com>
2022-10-23 22:53:11 +0530

    Docs for freee, README

--- Miscellaneous/freee.c
+++ Miscellaneous/freee.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
-#include "node.h"
+/** @file freee.c
+ *  @brief Contains definition for freee()
+ *  @author Saksham Attri
+ *  @bug No known bugs.
+ */
+
+/* -- Includes -- */
+
+/* libc includes */
+#include <stdio.h> /* for referencing to NULL */
+#include "node.h" /* for using the node struct*/
 
 int freee(node * temp){
     if (temp->left==NULL && temp->right==NULL){
-        free(temp);
+        free(temp); // frees the node if it has no children
     }
     else{
         if(temp->left!=NULL && temp->right==NULL){
--- Miscellaneous/freee.h
+++ Miscellaneous/freee.h
@@ -1,1 +1,12 @@
+/*! \file freee.h
+    \brief A documented header file
+    
+    Contains declaration for freee()
+*/
+/*! \fn  int freee(node * temp)
+    \brief The function frees allocated memory by in-order traversal.
+    \param temp The root node for binary tree.
+*/
+
+
 int freee(node * temp);
