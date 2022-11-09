053babe58a2409e4516c63848871510fcd976841
Saksham Attri <sakshamattri3@gmail.com>
2022-11-09 23:33:51 +0530

    Fix Bug#1

--- Documentation/html/search/all_7.js
+++ Documentation/html/search/all_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['judge_0',['judge',['../judge_8c.html#ac291604e53bd9b9fff8cdb29b1f2c417',1,'judge(node *temp):&#160;judge.c'],['../judge_8h.html#ac291604e53bd9b9fff8cdb29b1f2c417',1,'judge(node *temp):&#160;judge.c']]],
+  ['judge_0',['judge',['../judge_8c.html#adb79a38ba516f4ebc3cbbdbdc2312921',1,'judge(node *temp, char arr[]):&#160;judge.c'],['../judge_8h.html#a43cdc7425bf9959b91e4cbe8ce641ca4',1,'judge(node *temp, char a[]):&#160;judge.c']]],
   ['judge_2ec_1',['judge.c',['../judge_8c.html',1,'']]],
   ['judge_2eh_2',['judge.h',['../judge_8h.html',1,'']]]
 ];
